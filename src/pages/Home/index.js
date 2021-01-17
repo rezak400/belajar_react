ac8c74ed3b699f62021af30c1adfc468d013c447
rezak400 <k400ezza@gmail.com>
2021-01-17 22:19:34 +0700

    tes vercel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1>Hello dari halaman Home</h1>
+            <h1>Hello dari halaman Home tes vercel</h1>
         </div>
     )
 }
