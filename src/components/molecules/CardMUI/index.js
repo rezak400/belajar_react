bba84daf9365d60a3bc31f00c2d99cf115f643a1
rezak400 <k400ezza@gmail.com>
2021-01-21 20:05:01 +0700

    add blog-item

--- src/components/molecules/CardMUI/index.js
+++ src/components/molecules/CardMUI/index.js
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { LoginBg } from '../../../assets';
 
 const useStyles = makeStyles({
   root: {
@@ -25,16 +26,15 @@ export default function CardMUI({...rest}) {
       <CardActionArea {...rest}>
         <CardMedia
           className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image={LoginBg}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            Blog
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, repellendus aspernatur? Neque necessitatibus eaque illum eligendi natus reiciendis quas sint?
           </Typography>
         </CardContent>
       </CardActionArea>
