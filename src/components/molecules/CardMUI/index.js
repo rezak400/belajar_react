8cd7aeb7cc16b23f4c018fc2caab340ca29ca740
rezak400 <k400ezza@gmail.com>
2021-01-30 22:15:37 +0700

    remove lorem

--- src/components/molecules/CardMUI/index.js
+++ src/components/molecules/CardMUI/index.js
@@ -50,7 +50,6 @@ export default function CardMUI(props) {
                 <h2 class="text-2xl font-semibold text-gray-900 title-font mb-2 font-primary">{title}</h2>
                 <p class="leading-relaxed max-h-32 min-h-32 md:max-h-24 overflow-hidden font-secondary block">
                  {content}
-                 lorem60
                 </p>
                 <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
                   <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
